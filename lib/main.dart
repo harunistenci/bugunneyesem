8ea752054c3703689820f7bf37c95d7758255dd8
Harun İstenci <harunistenci@hotmail.com>
2023-07-19 16:07:45 +0300

    1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:core';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,12 +36,44 @@ class YemekSayfasi extends StatefulWidget {
 }
 
 class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbaNo = 1;
+  int yemekNo = 1;
+  int tatliNo = 1;
+
+  List<String> corbaAdlari = [
+    'Mercimek Çorbası',
+    'Tarhana',
+    'Tavuksuyu',
+    'Düğün Çorbası',
+    'Yoğurtlu Çorba'
+  ];
+
+  List<String> yemekAdlari = [
+    'Karnıyarık',
+    'Mantı',
+    'Kuru Fasulye',
+    'İçli Köfte',
+    'Izgara Balık'
+  ];
+
+  List<String> tatliAdlari = [
+    'Kadayıf',
+    'Baklava',
+    'Sütlaç',
+    'Kazandibi',
+    'Dondurma'
+  ];
+
+  void yemekleriYenile() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+      yemekNo = Random().nextInt(5) + 1;
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    int corbaNo = 1;
-    int yemekNo = 1;
-    int tatliNo = 1;
-
     return Center(
       child: Column(
         children: [
@@ -47,33 +82,52 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
               padding: const EdgeInsets.all(22.0),
               child: TextButton(
                 style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(EdgeInsets.all(5)),
                   overlayColor:
                       MaterialStateColor.resolveWith((states) => Colors.blue),
                   backgroundColor:
                       MaterialStateColor.resolveWith((states) => Colors.yellow),
                 ),
-                onPressed: () {
-                  print('$corbaNo Numaralı Çorba Aktif Edildi');
-                },
+                onPressed: yemekleriYenile,
                 child: Image.asset('assets/corba_$corbaNo.jpg'),
               ),
             ),
           ),
+          Text(
+            corbaAdlari[corbaNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+              width: 200,
+              child: Divider(
+                height: 5,
+                color: Colors.black,
+              )),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(22.0),
               child: TextButton(
                 style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(EdgeInsets.all(5)),
                   backgroundColor:
                       MaterialStateColor.resolveWith((states) => Colors.red),
                   overlayColor:
                       MaterialStateColor.resolveWith((states) => Colors.blue),
                 ),
-                onPressed: () {
-                  print('$yemekNo Numaralı Yemek Aktif Edildi');
-                },
+                onPressed: yemekleriYenile,
                 child: Image.asset('assets/yemek_$yemekNo.jpg'),
               ),
+            ),
+          ),
+          Text(
+            yemekAdlari[yemekNo - 1],
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
           Expanded(
@@ -81,16 +135,26 @@ class _YemekSayfasiState extends State<YemekSayfasi> {
               padding: const EdgeInsets.all(22.0),
               child: TextButton(
                 style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(EdgeInsets.all(5)),
                   backgroundColor:
                       MaterialStateColor.resolveWith((states) => Colors.green),
                   overlayColor:
                       MaterialStateColor.resolveWith((states) => Colors.blue),
                 ),
-                onPressed: () {
-                  print('$tatliNo Numaralı Tatlı Aktif Edildi');
-                },
+                onPressed: yemekleriYenile,
                 child: Image.asset('assets/tatli_$tatliNo.jpg'),
               ),
+            ),
+          ),
+          Text(
+            tatliAdlari[tatliNo - 1],
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          Container(
+            width: 200,
+            child: Divider(
+              height: 5,
+              color: Colors.black,
             ),
           ),
         ],
